fddb846c462689b65522d8bbd0aaf0da3a5e9e77
Leo <leo.pratama@aqi.co.id>
2024-09-04 15:52:01 +0700

    add: section skill, section contact, footer, animation scroll reveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,3 +34,39 @@ const bgHeader = () => {
 
 window.addEventListener('scroll', bgHeader)
 bgHeader()
+
+// Active link
+const section = document.querySelectorAll('section[id]')
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+
+    
+    section.forEach(current => {
+        const sectionTop = current.offsetTop - 58
+        const sectionHeight = current.offsetHeight
+        const sectionId = current.getAttribute('id')
+        const sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if (scrollDown > sectionTop && scrollDown < sectionTop + sectionHeight) {
+            sectionClass.classList.add('active-link')
+        } else {
+            sectionClass.classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
+
+// scroll reveal
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+})
+
+sr.reveal(`.hero__content, .skills__container, .projects__list, .contact__container`)
+sr.reveal(`.hero__img, .skills__img`, { delay: 1000, origin:'right' })
+sr.reveal(`.skills__list, .skill__data`, { delay: 1000, origin:'left' })
+sr.reveal(`.projects__info, .projects__card`, { interval: 500})
